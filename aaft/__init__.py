0b2e77bebb862573e0a9aa9fa47978ec61305b68
Larry Eisenman <Leisenman@wustl.edu>
2014-12-20 09:11:23 -0600

    add code

--- aaft/__init__.py
+++ aaft/__init__.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import (print_function, absolute_import,
+                        unicode_literals, division)
+import six
+
 from ._version import get_versions
 
 __version__ = get_versions()['version']
 del get_versions
+
+from .aaft import AAFTsur
